8d2bd54368893f6f8c84d66494c63c72e1cc4741
mitulbd <mitul.cse@gmail.com>
2017-09-26 23:09:34 +0600

    Installing Angular module +

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule}from '@angular/platform-browser/animations'
+import {NoopAnimationsModule}from '@angular/platform-browser/animations'
+import {MatCheckboxModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
